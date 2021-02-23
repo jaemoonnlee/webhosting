e56cfd6b0073f615c101aa6895bd6bcd89e929a4
jaemoonnlee <jaemoon.n.lee@gmail.com>
2021-02-23 16:02:52 +0900

    test saved

--- assignment/20210219/js/MyScript.js
+++ assignment/20210219/js/MyScript.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
     // ?
+    function show01() {
+        // **caution
+        // the code below is not working in local environment.
+        // it is working online because of policy.
+        $("#question").load("kakao2021_01_01_q.html");
+        $("#solve").load("kakao2021_01_01.html");
+    }
+    function show03() {
+        $("#question").load("kakao2021_01_03_q.html");
+        $("#solve").load("kakao2021_01_03.html");
+    }
 })
-
-function show01() {
-    // **caution
-    // the code below is not working in local environment.
-    // it is working online because of policy.
-    $("#question").load("kakao2021_01_01_q.html");
-    $("#solve").load("kakao2021_01_01.html");
-}
-function show03() {
-    $("#question").load("kakao2021_01_03_q.html");
-    $("#solve").load("kakao2021_01_03.html");
-}
